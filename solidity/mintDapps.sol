f59c1641dd086f2432c968c3aea2adfd9525f6dc
KouGa <kouga08082361184@gmail.com>
2022-10-05 14:16:22 +0900

    Changed mintToken argument so that it can be specified in units of wei

--- solidity/mintDapps.sol
+++ solidity/mintDapps.sol
@@ -18,30 +18,47 @@ contract TokenFaucet {
     ERC20 token;
     
     address owner;
+
+    uint256 value;
+
+    uint256 balance;
     
-    constructor (address _tokenAddress, address _ownerAddress) {
+    constructor (address _tokenAddress, address _ownerAddress, uint256 _value) {
         token = ERC20(_tokenAddress);
         owner = _ownerAddress;
+        value = _value;
     }
     
     modifier onlyOwner{
-        require(msg.sender == owner,"FaucetError: Caller not owner");
+        require(msg.sender == owner,"Error: Caller not owner");
         _;
     }
 
     function mintToken(uint256 _amount) payable external {
-        require(token.balanceOf(address(this)) > _amount, "FaucetError: ");
-        require(msg.value == 10**token.decimals() / 1000 * _amount, "FaucetError: ");
+        require(token.balanceOf(address(this)) >= _amount, "Error: ");
+        require(msg.value == _amount / 1000 * value, "Error: ");
         
-        token.transfer(msg.sender, _amount * 10**token.decimals());
+        balance += msg.value;
+        token.transfer(msg.sender, _amount);
     }
 
     function setTokenAddress(address _addToken) external onlyOwner {
         token = ERC20(_addToken);
     }
+
+    function setValue(uint256 _addValue) external onlyOwner {
+        value = _addValue ;
+    }
      
     function withdrawTokens(address _receiver, uint256 _amount) external onlyOwner {
-        require(token.balanceOf(address(this)) >= _amount,"FaucetError: Insufficient funds");
-        token.transfer(_receiver,_amount);
+        require(token.balanceOf(address(this)) >= _amount,"Error: ");
+        token.transfer(_receiver, _amount);
     }
+
+    function withdraw(uint256 _amount) external onlyOwner {
+        require(balance >= _amount, "Error: ");
+        balance -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+
 }
